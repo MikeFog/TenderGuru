c8e5af001dcc33df937eb0d4eb73c38e55c7c03d
MikeFog <fogg_@hotmail.com>
2024-12-01 13:36:42 -0300

    Flask support added

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print('hello')
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Привет, мир!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
